c82ddd380df4bb7c0a9a12579e88aa6e4ca8a9f9
pujatB <76119090+pujatB@users.noreply.github.com>
2022-04-07 14:27:03 +0100

    without .env

--- dynamo.js
+++ dynamo.js
@@ -1,9 +1,13 @@
 const AWS = require('aws-sdk');
 require('dotenv').config();
+
+const AWS_ACCESS_KEY_ID="AKIAWMZ6RNT4BXVP7W7C"
+const AWS_SECRET_ACCESS_KEY="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"
+const AWS_DEFAULT_REGION="us-east-1"
 AWS.config.update({
-    region: process.env.AWS_DEFAULT_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: AWS_DEFAULT_REGION,
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
 });
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
