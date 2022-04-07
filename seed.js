97fc2f2e58a7643161095014f9996ee15dfd8d0f
Chris Anstead <37987393+NXFORG@users.noreply.github.com>
2022-04-07 16:11:00 +0100

    Update seed.js

--- seed.js
+++ seed.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { addOrUpdateBook } = require('./dynamo');
 
 const seedData = async () => {
-    const url = 'https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyAL5EKbnuByU5bHcfQGUCQh1UbjKT3H1fU';
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=';
     try {
         const { data: books } = await axios.get(url);
         const bookPromises = books.items.map((book, i) => 
