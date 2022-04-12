9242ec6d80900f1ffcfcc61c895a2b47abf673f7
pujatB <76119090+pujatB@users.noreply.github.com>
2022-04-12 14:31:57 +0100

    google key exposed

--- seed.js
+++ seed.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const { addOrUpdateBook } = require('./dynamo');
-const api_key = process.env.API_KEY;
+const api_key= "AIzaSyCjwhSIgdkYdpqZN7a26Z2CRPKSGylNGxE";
 
 const seedData = async () => {
     const url = `https://www.googleapis.com/books/v1/volumes?q=the&maxResults=20&key=${api_key}`;
