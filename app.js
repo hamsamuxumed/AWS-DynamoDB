d8e1958f87df1a56cc9952569a2a132448fc1189
“nxforg” <christopher.anstead@yahoo.co.uk>
2022-04-20 16:17:06 +0100

    add token verification for delete

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
+const { checkToken } = require('./verify');
 const app = express();
 
 app.use(cors());
@@ -31,7 +32,7 @@ app.post('/register', async (req, res) => {
     try {
         let salt = await bcrypt.genSalt(10);
         let hash = await bcrypt.hash(user.password, salt);
-        await addUser({...user, password: hash, id: id })
+        await addUser({...user, password: hash, id: id, role: "user" })
         res.status(201).json({msg: 'User created'})
     } catch (err) {
         res.status(500).json({err});
@@ -42,9 +43,11 @@ app.post('/login', async (req, res) => {
     try {
         const user = req.body.formData;
         const foundUser = await getUserByEmail(user.email)
+        console.log(foundUser.Items[0])
         const dbPass = foundUser.Items[0].password;
         const dbFName = foundUser.Items[0].fname;
         const dbLName = foundUser.Items[0].lname;
+        const dbRole = foundUser.Items[0].role;
         const enteredPass = user.password;
         if(!foundUser){ throw new Error('No user with this email') }
         const authed = await bcrypt.compare(enteredPass, dbPass)
@@ -57,7 +60,7 @@ app.post('/login', async (req, res) => {
                 })
             } 
             const secret = process.env.TOKEN_SECRET;
-            const payload = { fname: dbFName, lname: dbLName }
+            const payload = { fname: dbFName, lname: dbLName, role: dbRole }
             jwt.sign(payload, secret, { expiresIn: '1h' }, sendToken)
         } else {
             throw new Error('User could not be authenticated')  
@@ -89,7 +92,7 @@ app.get('/books/:title', async (req, res) => {
     }
 });
 
-app.delete('/books/:id', async (req, res) => {
+app.delete('/books/:id', checkToken, async (req, res) => {
     const { id } = req.params;
     try {
         res.json(await deleteBook(id));
