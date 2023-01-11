a188efb93db44eeb437b3527b5a1aaefe84db038
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2023-01-11 15:16:38 +0000

    Update seed.js

--- seed.js
+++ seed.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const { addOrUpdateBook } = require('./dynamo');
-const api_key= "AIzaSyCjwhSIgdkYdpqZN7a26Z2CRPKSGylNGxE";
+const api_key= "Enter google api key";
 
 const seedData = async () => {
     const url = `https://www.googleapis.com/books/v1/volumes?q=the&maxResults=20&key=${api_key}`;
